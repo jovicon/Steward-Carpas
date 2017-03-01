2857a636066a715f94461628f0a2f2c27693f348
jovicon <jocontrera16@gmail.com>
2017-03-01 05:24:51 -0300

    buen efecto de imagenes y botones

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function moveImage0(){
+  document.miFormulario.miCheck0.click()
+}
+
+function moveImage1(){
+  document.miFormulario.miCheck1.click()
+}
+
+function moveImage2(){
+  document.miFormulario.miCheck2.click()
+}
+
+function moveImage3(){
+  document.miFormulario.miCheck3.click()
+}
+
+function moveImage4(){
+  document.miFormulario.miCheck4.click()
+}
