8506ca9bbaae48baf8d42db7b6cfb71cb214b806
jovicon <jocontrera16@gmail.com>
2017-03-03 02:11:50 -0300

    paso 2 terminado

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// funcionamiento de botones slider
+
 function moveImage0(){
   document.miFormulario.miCheck0.click()
 }
@@ -17,3 +19,57 @@ function moveImage3(){
 function moveImage4(){
   document.miFormulario.miCheck4.click()
 }
+
+// end funcionamiento botones slider
+
+
+// agregar clase a imaegenes
+
+$( '#checkbox01' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img01").addClass("img_hidden");
+    } else {
+        $("#img01").removeClass("img_hidden");
+    }
+});
+
+$( '#checkbox02' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img02").addClass("img_hidden");
+    } else {
+        $("#img02").removeClass("img_hidden");
+    }
+});
+
+$( '#checkbox03' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img03").addClass("img_hidden");
+    } else {
+        $("#img03").removeClass("img_hidden");
+    }
+});
+
+$( '#checkbox04' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img04").addClass("img_hidden");
+    } else {
+        $("#img04").removeClass("img_hidden");
+    }
+});
+
+$( '#checkbox05' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img05").addClass("img_hidden");
+    } else {
+        $("#img05").removeClass("img_hidden");
+    }
+});
+
+$( '#checkbox06' ).click(function() {
+    if( $(this).is(':checked') ){
+        $("#img06").addClass("img_hidden");
+    } else {
+        $("#img06").removeClass("img_hidden");
+    }
+});
+// end agregar clases a imagenes
